ebd50b842f297122442e8f3aba74053885e3ccb3
mariedeiub <mariedeiub@gmail.com>
2022-11-01 20:10:24 -0300

    formularios y animaciones

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const index = "./views/home.html"
-let PORT = 3030;
+const register = "./views/register.html"
+const login = "./views/login.html"
+let PORT = process.env.PORT || 3000;
+
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log("Servidor corriendo en el puerto: " + PORT));
+app.listen(PORT, () => console.log("Servidor corriendo en el Puerto: " + PORT));
 
 
 app.get('/', (req, res) => {
-    //res.send("Primer servidor");
     res.sendFile(path.resolve(index));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(register));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(login));
 })
 
 // ERROR 404 Not found
 app.get("/*", (req, res) => {
-    res.status(404).send("404 NOT FOUND");
+    res.status(404).send("404 - NOT FOUND");
 })
